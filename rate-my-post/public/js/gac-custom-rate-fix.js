95da8a9949f62e0d924be55e026bbb51aa7da1ba
Daniel O'Leary <danpoleary@netscape.net>
2022-04-01 10:08:00 -0400

    Added jquery code
    This jquery sccript code needs it contents copied into the "Settings/Insert Headers & Footers/Scripts in Header" section to allow dynamic page load to aalso load the widget.

---
+++ rate-my-post/public/js/gac-custom-rate-fix.js
@@ -0,0 +1,12 @@
+<script>jQuery(function($){
+  $('body').on( 'alnp-post-loaded', function( e, params ) {
+    RateMyPost.re_init();
+  });
+});
+</script>
+<script>jQuery(function($){
+  $('body').on( 'post-load', function( e, params ) {
+    RateMyPost.re_init();
+  });
+});
+</script>
